0adfbc7dd9311f6897037480fdb1885d4b2a721d
Katy Ashworth <katyashworth27@gmail.com>
2025-01-29 18:52:24 +0000

    Create generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kashdotcom-djangoblog-g0ngsh1r8pm.ws.codeinstitute-ide.net', '.herokuapp.com']
 
