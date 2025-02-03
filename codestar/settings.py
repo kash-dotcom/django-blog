165b80224b1fb78a1f8dfcd120685cf0f76fcfce
kash-dotcom <katyashworth27@gmail.com>
2025-02-03 20:22:00 +0000

    enable serving of static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -55,9 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Templates
 TEMPLATES = [
