80bbf14831ae3ac460803a22f56d5330953b3f6c
kash-dotcom <katyashworth27@gmail.com>
2025-02-10 22:46:51 +0000

    Corrected spelling error in Post Model (featured_image)

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="blog_posts")
-    featureed_image = CloudinaryField("image", default="image/upload/v1629780000/default.jpg")
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
